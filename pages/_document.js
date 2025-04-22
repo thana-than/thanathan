ea7f2147ea1032bbe4987ec6c5d84875b9b13991
Thanathan <thanthemannn@gmail.com>
2025-04-22 01:08:40 -0400

    BASIC css

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,9 @@ export default function Document() {
       <Head>
         <meta name="darkreader-lock" />
         <meta name="color-scheme" content="dark" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&family=Space+Grotesk:wght@300..700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
