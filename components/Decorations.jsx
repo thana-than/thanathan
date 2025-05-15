bdd6b8d6041be8f9310ce8612665293b23516dce
Than <thanthemannn@gmail.com>
2025-05-15 15:11:48 -0400

    Added decorations component

---
+++ components/Decorations.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShootingStar from '@/components/ShootingStar.jsx';
+
+const Decorations = () => {
+    return (
+        <>
+            {/* <ShootingStar href="https://fuckeduplittleguy.com" /> */}
+        </>
+    );
+}
+
+export default Decorations;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+import Decorations from '@/components/Decorations';
 import Layout from '@/components/Layout';
 import '@/css/styles.css'
 
 export default function MyApp({ Component, pageProps }) {
     return (
-        <Layout pageProps={pageProps}>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <Layout pageProps={pageProps}>
+                <Component {...pageProps} />
+            </Layout>
+            <Decorations />
+        </>
     );
 }
