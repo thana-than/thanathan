03ed7a772290ed71df42eb6504a5c165586a5160
Than <thanthemannn@gmail.com>
2025-05-15 19:04:39 -0400

    removed breaking Decorations code

--- components/Decorations.jsx
+++ components/Decorations.jsx
@@ -4,21 +4,23 @@ import { Canvas } from '@react-three/fiber'
 
 const Decorations = () => {
     return (
-        <div style={{
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            overflow: 'clip'
-        }}>
-            <div style={{
-                height: '1000px',
-            }}>
-                <Canvas>
-                    <ambientLight />
-                    {/* <ShootingStar href="https://fuckeduplittleguy.com" scale=".05" speed="1" /> */}
-                </Canvas>
-            </div>
-        </div>
+        //TODO right now the commented out div is stopping click throughs. Fix this before proceeding
+        <></>
+        // <div style={{
+        //     position: 'absolute',
+        //     width: '100%',
+        //     height: '100%',
+        //     overflow: 'clip'
+        // }}>
+        //     <div style={{
+        //         height: '1000px',
+        //     }}>
+        //         <Canvas>
+        //             <ambientLight />
+        //             <ShootingStar href="https://fuckeduplittleguy.com" scale=".05" speed="1" />
+        //         </Canvas>
+        //     </div>
+        // </div>
     );
 }
 
